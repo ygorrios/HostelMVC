e1d6d4dd8cc5dd6c3c28778c0619bed941688b74
ygorrios <ygor.rios@outlook.com>
2017-02-17 19:09:28 +0000

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Camada.Aplicacao.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Camada.Aplicacao.Startup))]
+namespace Camada.Aplicacao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
